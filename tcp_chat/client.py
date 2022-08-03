663d3c7738666a62ef82e80a0e6e14bef5420dfe
Aubrey Clark <aubs.bc@gmail.com>
2022-08-03 09:26:41 -0700

    chat between clients

--- tcp_chat/client.py
+++ tcp_chat/client.py
@@ -1,6 +1,8 @@
 # client.py
 import socket
 import struct
+import threading
+import sys
 
 HOST = '127.0.0.1'
 PORT = 65432
@@ -11,55 +13,59 @@ def send_message(sock, message):
     sock.sendall(length_prefix + message_bytes)
 
 def receive_message(sock):
-    raw_length = sock.recv(4)
-    if not raw_length:
-        return None
-    
-    message_length = struct.unpack('>I', raw_length)[0]
-    
-    full_message = bytearray()
-    while len(full_message) < message_length:
-        remaining_bytes = message_length - len(full_message)
-        chunk = sock.recv(min(4096, remaining_bytes))
-        if not chunk:
+    try:
+        raw_length = sock.recv(4)
+        if not raw_length:
             return None
-        full_message.extend(chunk)
-    
-    return full_message.decode()
+        
+        message_length = struct.unpack('>I', raw_length)[0]
+        
+        full_message = bytearray()
+        while len(full_message) < message_length:
+            remaining_bytes = message_length - len(full_message)
+            chunk = sock.recv(min(4096, remaining_bytes))
+            if not chunk:
+                return None
+            full_message.extend(chunk)
+        
+        return full_message.decode()
+    except (ConnectionResetError, struct.error):
+        return None
+
+def receive_handler(sock):
+    while True:
+        message = receive_message(sock)
+        if message is None:
+            print("\rConnection to server lost. Press Enter to exit.", flush=True)
+            break
+        print(f"\r{message}\nEnter message: ", end="", flush=True)
 
 def main():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         client_socket.connect((HOST, PORT))
-        print(f"Connected to server at {HOST}:{PORT}")
+        print(f"Connected to server at {HOST}:{PORT}. Type 'quit' to exit.", flush=True)
+
+        receiver = threading.Thread(target=receive_handler, args=(client_socket,))
+        receiver.daemon = True
+        receiver.start()
 
         while True:
-            user_input = input("Enter message (or 'quit' to exit, 'long' for test message): ")
-            if user_input.lower() == 'quit':
+            message_to_send = input() 
+            if message_to_send.lower() == 'quit':
                 break
             
-            message_to_send = user_input
-            if user_input.lower() == 'long':
-                long_test_string = "long test message designed to be significantly longer than 1024 bytes"
-                message_to_send = long_test_string * 50
-                print(f"Sending a long test message of {len(message_to_send)} bytes.")
-
-            send_message(client_socket, message_to_send)
-            
-            received_message = receive_message(client_socket)
-            if received_message is None:
-                print("Server closed the connection unexpectedly.")
+            if receiver.is_alive():
+                send_message(client_socket, message_to_send)
+            else:
                 break
-            print(f"Received from server: {received_message}")
 
     except ConnectionRefusedError:
-        print("Connection refused: Is the server running?")
-    except struct.error:
-        print("Error unpacking message length. Server might have sent malformed data or disconnected.")
+        print("Connection refused: Is the server running?", flush=True)
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred: {e}", flush=True)
     finally:
-        print("Closing client socket.")
+        print("Closing client socket.", flush=True)
         client_socket.close()
 
 if __name__ == "__main__":
--- tcp_chat/server.py
+++ tcp_chat/server.py
@@ -2,59 +2,102 @@
 import socket
 import threading
 import struct
+import time
 
 HOST = '127.0.0.1'
 PORT = 65432
 
+clients = []
+clients_lock = threading.Lock()
+
 def send_message(sock, message):
     message_bytes = message.encode()
     length_prefix = struct.pack('>I', len(message_bytes))
-    sock.sendall(length_prefix + message_bytes)
+    try:
+        sock.sendall(length_prefix + message_bytes)
+    except (BrokenPipeError, ConnectionResetError):
+        return False
+    return True
 
 def receive_message(sock):
-    raw_length = sock.recv(4)
-    if not raw_length:
-        return None
-    
-    message_length = struct.unpack('>I', raw_length)[0]
-    
-    full_message = bytearray()
-    while len(full_message) < message_length:
-        remaining_bytes = message_length - len(full_message)
-        chunk = sock.recv(min(4096, remaining_bytes))
-        if not chunk:
+    try:
+        raw_length = sock.recv(4)
+        if not raw_length:
             return None
-        full_message.extend(chunk)
-    
-    return full_message.decode()
+        
+        message_length = struct.unpack('>I', raw_length)[0]
+        
+        full_message = bytearray()
+        while len(full_message) < message_length:
+            remaining_bytes = message_length - len(full_message)
+            chunk = sock.recv(min(4096, remaining_bytes))
+            if not chunk:
+                return None
+            full_message.extend(chunk)
+        
+        return full_message.decode()
+    except (ConnectionResetError, BrokenPipeError, struct.error):
+        return None
+
+def broadcast(message, sender_conn):
+    with clients_lock:
+        for client_conn in list(clients):
+            if client_conn != sender_conn:
+                if not send_message(client_conn, message):
+                    clients.remove(client_conn)
+                    print(f"Removed disconnected client during broadcast.", flush=True)
 
 def client_handler(conn, addr):
-    print(f"Connected by {addr}", flush=True)
-    with conn:
+    with clients_lock:
+        clients.append(conn)
+    
+    broadcast_message = f"User {addr} has joined the chat!"
+    broadcast(broadcast_message, None)
+    print(broadcast_message, flush=True)
+
+    try:
         while True:
-            try:
-                received_message = receive_message(conn)
-                if received_message is None:
-                    break
-                
-                print(f"Received from client {addr}: {received_message}", flush=True)
-                send_message(conn, received_message)
-                
-            except (ConnectionResetError, BrokenPipeError, struct.error):
+            received_message = receive_message(conn)
+            if received_message is None:
                 break
-    print(f"Client {addr} disconnected.", flush=True)
+            
+            full_message_to_broadcast = f"[{addr}] {received_message}"
+            print(f"Broadcasting: {full_message_to_broadcast}", flush=True)
+            broadcast(full_message_to_broadcast, conn)
+            
+    finally:
+        with clients_lock:
+            if conn in clients:
+                clients.remove(conn)
+        
+        broadcast_message = f"User {addr} has left the chat."
+        broadcast(broadcast_message, None)
+        print(broadcast_message, flush=True)
+        conn.close()
+        
+    print(f"Client {addr} handler finished.", flush=True)
+
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((HOST, PORT))
     server_socket.listen()
     print(f"Server is listening on {HOST}:{PORT}...", flush=True)
 
-    while True:
-        conn, addr = server_socket.accept()
-        thread = threading.Thread(target=client_handler, args=(conn, addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}", flush=True)
+    try:
+        while True:
+            conn, addr = server_socket.accept()
+            thread = threading.Thread(target=client_handler, args=(conn, addr))
+            thread.daemon = True
+            thread.start()
+            with clients_lock:
+                print(f"[ACTIVE CONNECTIONS] {len(clients)}", flush=True)
+    except KeyboardInterrupt:
+        print("Server shutting down gracefully...", flush=True)
+    finally:
+        server_socket.close()
+        print("Server stopped.", flush=True)
 
 if __name__ == "__main__":
     main()
