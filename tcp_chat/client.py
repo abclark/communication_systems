b715051d8cdb3443e7ad410c8cf9b1c02a3bb7be
Aubrey Clark <aubs.bc@gmail.com>
2022-08-04 10:12:45 -0700

    transport layer security for tcp_chat

--- tcp_chat/client.py
+++ tcp_chat/client.py
@@ -3,6 +3,8 @@ import socket
 import struct
 import threading
 import sys
+import ssl
+import os
 
 HOST = '127.0.0.1'
 PORT = 65432
@@ -41,12 +43,29 @@ def receive_handler(sock):
         print(f"\r{message}\nEnter message: ", end="", flush=True)
 
 def main():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        client_socket.connect((HOST, PORT))
-        print(f"Connected to server at {HOST}:{PORT}. Type 'quit' to exit.", flush=True)
+    # Get the absolute path to the directory where the script is located
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    cert_path = os.path.join(script_dir, 'ssl', 'cert.pem')
 
-        receiver = threading.Thread(target=receive_handler, args=(client_socket,))
+    # Create an SSL context that trusts our self-signed certificate
+    context = ssl.create_default_context(
+        ssl.Purpose.SERVER_AUTH, cafile=cert_path)
+    # Since we're using a self-signed certificate with a custom hostname
+    # (localhost), we may need to disable standard hostname checking.
+    # For a real application, you would use a proper certificate with a
+    # matching hostname.
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_REQUIRED
+
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    secure_sock = context.wrap_socket(client_socket, server_hostname=HOST)
+
+    try:
+        secure_sock.connect((HOST, PORT))
+        print(f"Connected to secure server at {HOST}:{PORT}. Type 'quit' to exit.", flush=True)
+
+        receiver = threading.Thread(target=receive_handler, args=(secure_sock,))
         receiver.daemon = True
         receiver.start()
 
@@ -56,17 +75,20 @@ def main():
                 break
             
             if receiver.is_alive():
-                send_message(client_socket, message_to_send)
+                send_message(secure_sock, message_to_send)
             else:
                 break
 
     except ConnectionRefusedError:
         print("Connection refused: Is the server running?", flush=True)
+    except ssl.SSLCertVerificationError as e:
+        print(f"Certificate verification error: {e}", flush=True)
+        print("Ensure the client trusts the server's certificate.", flush=True)
     except Exception as e:
         print(f"An error occurred: {e}", flush=True)
     finally:
         print("Closing client socket.", flush=True)
-        client_socket.close()
+        secure_sock.close()
 
 if __name__ == "__main__":
     main()
--- tcp_chat/server.py
+++ tcp_chat/server.py
@@ -3,6 +3,8 @@ import socket
 import threading
 import struct
 import time
+import ssl
+import os
 
 HOST = '127.0.0.1'
 PORT = 65432
@@ -79,15 +81,25 @@ def client_handler(conn, addr):
 
 
 def main():
+    # Get the absolute path to the directory where the script is located
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    cert_path = os.path.join(script_dir, 'ssl', 'cert.pem')
+    key_path = os.path.join(script_dir, 'ssl', 'key.pem')
+
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.load_cert_chain(certfile=cert_path, keyfile=key_path)
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((HOST, PORT))
     server_socket.listen()
     print(f"Server is listening on {HOST}:{PORT}...", flush=True)
 
+    secure_server_socket = context.wrap_socket(server_socket, server_side=True)
+
     try:
         while True:
-            conn, addr = server_socket.accept()
+            conn, addr = secure_server_socket.accept()
             thread = threading.Thread(target=client_handler, args=(conn, addr))
             thread.daemon = True
             thread.start()
@@ -96,7 +108,7 @@ def main():
     except KeyboardInterrupt:
         print("Server shutting down gracefully...", flush=True)
     finally:
-        server_socket.close()
+        secure_server_socket.close()
         print("Server stopped.", flush=True)
 
 if __name__ == "__main__":
