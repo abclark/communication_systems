a6c0502c0b52aa409304b96303cba7b76286b09a
abclark <aubs.bc@gmail.com>
2022-08-25 14:32:11 -0700

    echo reply logic for response to pings

--- tcp_ip_stack/stack.py
+++ tcp_ip_stack/stack.py
@@ -110,14 +110,43 @@ if __name__ == '__main__':
                     print("--- PARSED IP HEADER ---")
                     print(ip_header)
                     
-                    # Calculate where the IP header ends, and the payload (ICMP) begins.
                     ip_header_length = ip_header.ihl * 4
 
-                    if ip_header.protocol == 1: # Protocol 1 is ICMP
+                    if ip_header.protocol == 1:
                         icmp_bytes = packet_bytes[ip_header_length:]
                         icmp_msg = ICMPMessage.from_bytes(icmp_bytes)
                         print("--- PARSED ICMP MESSAGE ---")
                         print(icmp_msg)
+
+                        if icmp_msg.type == 8:
+                            print("   >>> Sending Echo Reply...")
+                            
+                            reply_icmp = ICMPMessage(
+                                type=0, 
+                                code=0, 
+                                checksum=0,
+                                identifier=icmp_msg.identifier, 
+                                sequence_number=icmp_msg.sequence_number, 
+                                payload=icmp_msg.payload
+                            )
+                            reply_icmp_bytes = reply_icmp.to_bytes()
+
+                            reply_ip = IPHeader(
+                                version=4,
+                                ihl=5,
+                                tos=0,
+                                total_length=20 + len(reply_icmp_bytes),
+                                identification=0,
+                                flags_offset=0,
+                                ttl=64,
+                                protocol=1,
+                                checksum=0,
+                                src_ip=ip_header.dest_ip,
+                                dest_ip=ip_header.src_ip
+                            )
+                            reply_ip_bytes = reply_ip.to_bytes()
+
+                            tun.write(reply_ip_bytes + reply_icmp_bytes)
                     
                     print(f"Raw Packet Length: {len(packet_bytes)} bytes\n")
                 except ValueError as e:
