9661662d7cfe2d1302c2aa6af4e41c623f3cff44
abclark <aubs.bc@gmail.com>
2022-08-26 15:42:18 -0700

    refactor: restructuring stack.py into TCP_IP_Stack class and handler methods

--- tcp_ip_stack/stack.py
+++ tcp_ip_stack/stack.py
@@ -89,13 +89,13 @@ class TunDevice:
         pass
 
 
-if __name__ == '__main__':
-    print("--- Starting Stack Skeleton ---")
-    tun = None 
-    try:
-        tun = TunDevice()
-        print("Stack is running (Press Ctrl+C to stop)...")
-        
+class TCP_IP_Stack:
+    def __init__(self):
+        print("TCP_IP_Stack: Initializing...")
+        self.tun = TunDevice()
+
+    def run(self):
+        print("TCP_IP_Stack: Starting main loop...")
         print("\n--- ACTION REQUIRED ---")
         print("1. Find the name of the new utun interface by running: 'ifconfig' in a separate terminal.")
         print("2. Configure it: 'sudo ifconfig utunX 10.0.0.1 10.0.0.1 netmask 255.255.255.0 up'")
@@ -103,61 +103,80 @@ if __name__ == '__main__':
         print("------------------------------------------------------------------\n")
 
         while True:
-            packet_bytes = tun.read()
+            packet_bytes = self.tun.read()
             if packet_bytes:
-                try:
-                    ip_header = IPHeader.from_bytes(packet_bytes)
-                    print("--- PARSED IP HEADER ---")
-                    print(ip_header)
-                    
-                    ip_header_length = ip_header.ihl * 4
+                self._handle_packet(packet_bytes)
 
-                    if ip_header.protocol == 1:
-                        icmp_bytes = packet_bytes[ip_header_length:]
-                        icmp_msg = ICMPMessage.from_bytes(icmp_bytes)
-                        print("--- PARSED ICMP MESSAGE ---")
-                        print(icmp_msg)
-
-                        if icmp_msg.type == 8:
-                            print("   >>> Sending Echo Reply...")
-                            
-                            reply_icmp = ICMPMessage(
-                                type=0, 
-                                code=0, 
-                                checksum=0,
-                                identifier=icmp_msg.identifier, 
-                                sequence_number=icmp_msg.sequence_number, 
-                                payload=icmp_msg.payload
-                            )
-                            reply_icmp_bytes = reply_icmp.to_bytes()
-
-                            reply_ip = IPHeader(
-                                version=4,
-                                ihl=5,
-                                tos=0,
-                                total_length=20 + len(reply_icmp_bytes),
-                                identification=0,
-                                flags_offset=0,
-                                ttl=64,
-                                protocol=1,
-                                checksum=0,
-                                src_ip=ip_header.dest_ip,
-                                dest_ip=ip_header.src_ip
-                            )
-                            reply_ip_bytes = reply_ip.to_bytes()
-
-                            tun.write(reply_ip_bytes + reply_icmp_bytes)
-                    
-                    print(f"Raw Packet Length: {len(packet_bytes)} bytes\n")
-                except ValueError as e:
-                    print(f"Error parsing IP/ICMP header: {e}", file=sys.stderr)
-                    print(f"Raw bytes: {packet_bytes.hex()}\n")
+    def _handle_packet(self, packet_bytes):
+        try:
+            ip_header = IPHeader.from_bytes(packet_bytes)
+            print("--- PARSED IP HEADER ---")
+            print(ip_header)
             
+            ip_header_length = ip_header.ihl * 4
+
+            if ip_header.protocol == 1:
+                icmp_bytes = packet_bytes[ip_header_length:]
+                self._handle_icmp(ip_header, icmp_bytes)
+            
+            print(f"Raw Packet Length: {len(packet_bytes)} bytes\n")
+        except ValueError as e:
+            print(f"Error parsing IP header: {e}", file=sys.stderr)
+            print(f"Raw bytes: {packet_bytes.hex()}\n")
+
+    def _handle_icmp(self, ip_header, icmp_bytes):
+        try:
+            icmp_msg = ICMPMessage.from_bytes(icmp_bytes)
+            print("--- PARSED ICMP MESSAGE ---")
+            print(icmp_msg)
+
+            if icmp_msg.type == 8:
+                print("   >>> Sending Echo Reply...")
+                
+                reply_icmp = ICMPMessage(
+                    type=0, 
+                    code=0, 
+                    checksum=0,
+                    identifier=icmp_msg.identifier, 
+                    sequence_number=icmp_msg.sequence_number, 
+                    payload=icmp_msg.payload
+                )
+                reply_icmp_bytes = reply_icmp.to_bytes()
+
+                reply_ip = IPHeader(
+                    version=4,
+                    ihl=5,
+                    tos=0,
+                    total_length=20 + len(reply_icmp_bytes),
+                    identification=0,
+                    flags_offset=0,
+                    ttl=64,
+                    protocol=1,
+                    checksum=0,
+                    src_ip=ip_header.dest_ip,
+                    dest_ip=ip_header.src_ip
+                )
+                reply_ip_bytes = reply_ip.to_bytes()
+
+                self.tun.write(reply_ip_bytes + reply_icmp_bytes)
+        except ValueError as e:
+            print(f"Error parsing ICMP message: {e}", file=sys.stderr)
+
+    def close(self):
+        self.tun.close()
+
+
+if __name__ == '__main__':
+    print("--- Starting Stack ---")
+    stack = None
+    try:
+        stack = TCP_IP_Stack()
+        stack.run()
     except KeyboardInterrupt:
         print("\n--- Ctrl+C detected. ---")
     except Exception as e:
         print(f"--- An error occurred: {e} ---", file=sys.stderr)
     finally:
-        if tun:
-            tun.close()
+        if stack:
+            stack.close()
         print("--- Stack shut down. ---")
