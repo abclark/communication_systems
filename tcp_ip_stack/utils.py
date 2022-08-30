b6223118d8f2379078e9c6c76227995e233a1bcd
abclark <aubs.bc@gmail.com>
2022-08-30 15:26:41 -0700

    feat: UDP checksum

--- tcp_ip_stack/utils.py
+++ tcp_ip_stack/utils.py
@@ -1,3 +1,6 @@
+import struct
+import socket
+
 def calculate_checksum(data):
     if len(data) % 2 == 1:
         data += b'\x00'
@@ -13,3 +16,22 @@ def calculate_checksum(data):
     checksum = ~s & 0xFFFF
     
     return checksum
+
+def calculate_udp_checksum(src_ip, dest_ip, udp_packet):
+    src_ip_bytes = socket.inet_aton(src_ip)
+    dest_ip_bytes = socket.inet_aton(dest_ip)
+    reserved = 0
+    protocol = 17
+    udp_length = len(udp_packet)
+    
+    pseudo_header = struct.pack('!4s4sBBH',
+        src_ip_bytes,
+        dest_ip_bytes,
+        reserved,
+        protocol,
+        udp_length
+    )
+    
+    full_data = pseudo_header + udp_packet
+    
+    return calculate_checksum(full_data)
