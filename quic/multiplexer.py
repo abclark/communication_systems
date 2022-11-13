6528bff94817a72bef97d7a8094b860dc5e17b85
abclark <aubs.bc@gmail.com>
2022-11-12 18:14:09 -0800

    feat: respond to SYN with SYN-ACK, completing TCP handshake

--- quic/multiplexer.py
+++ quic/multiplexer.py
@@ -1,11 +1,44 @@
 import sys
 sys.path.insert(0, '../tcp_ip_stack')
 
+import random
 from stack import TunDevice
 from packet_headers import IPHeader, TCPHeader
 import protocols
 
 STREAM_PORTS = [9001, 9002, 9003]
+
+
+def send_tcp(tun, src_ip, src_port, dest_ip, dest_port, seq, ack, flags):
+    tcp = TCPHeader(
+        src_port=src_port,
+        dest_port=dest_port,
+        seq_num=seq,
+        ack_num=ack,
+        flags=flags,
+        window=65535,
+        checksum=0,
+        urgent_ptr=0,
+        payload=b''
+    )
+    tcp_bytes = tcp.to_bytes(src_ip, dest_ip)
+
+    ip = IPHeader(
+        version=4,
+        ihl=5,
+        tos=0,
+        total_length=20 + len(tcp_bytes),
+        identification=0,
+        flags_offset=0,
+        ttl=64,
+        protocol=protocols.PROTO_TCP,
+        checksum=0,
+        src_ip=src_ip,
+        dest_ip=dest_ip
+    )
+    ip_bytes = ip.to_bytes()
+    tun.write(ip_bytes + tcp_bytes)
+
 
 def main():
     tun = TunDevice()
@@ -23,7 +56,25 @@ def main():
                 tcp_header = TCPHeader.from_bytes(tcp_bytes)
 
                 if tcp_header.dest_port in STREAM_PORTS:
-                    print(f"Port {tcp_header.dest_port}: {tcp_header}")
+                    is_syn = (tcp_header.flags & protocols.TCP_FLAG_SYN) and not (tcp_header.flags & protocols.TCP_FLAG_ACK)
+
+                    if is_syn:
+                        our_seq = random.randint(0, 2**32 - 1)
+                        our_ack = tcp_header.seq_num + 1
+
+                        send_tcp(
+                            tun,
+                            src_ip=ip_header.dest_ip,
+                            src_port=tcp_header.dest_port,
+                            dest_ip=ip_header.src_ip,
+                            dest_port=tcp_header.src_port,
+                            seq=our_seq,
+                            ack=our_ack,
+                            flags=protocols.TCP_FLAG_SYN | protocols.TCP_FLAG_ACK
+                        )
+                        print(f"Port {tcp_header.dest_port}: SYN received, sent SYN-ACK")
+                    else:
+                        print(f"Port {tcp_header.dest_port}: {tcp_header}")
 
 if __name__ == '__main__':
     try:
