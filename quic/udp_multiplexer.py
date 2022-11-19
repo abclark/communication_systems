22a6214a14fa79dc8b1d34141a67bd088b491840
abclark <aubs.bc@gmail.com>
2022-11-18 18:07:54 -0800

    feat: per-stream sequences with independent delivery

--- quic/udp_multiplexer.py
+++ quic/udp_multiplexer.py
@@ -7,6 +7,11 @@ import protocols
 
 UDP_PORT = 9000
 
+stream_seqs = {1: 1, 2: 1, 3: 1}
+stream_next_deliver = {1: 1, 2: 1, 3: 1}
+stream_pending = {1: [], 2: [], 3: []}
+delayed_messages = []
+
 
 def main():
     tun = TunDevice()
@@ -28,7 +33,32 @@ def main():
                     if len(payload) >= 1:
                         stream_id = payload[0]
                         data = payload[1:].decode('utf-8', errors='replace').strip()
-                        print(f"[Stream {stream_id}] Data: {data}")
+
+                        if stream_id not in stream_seqs:
+                            continue
+
+                        seq = stream_seqs[stream_id]
+                        stream_seqs[stream_id] += 1
+
+                        if data == "flush":
+                            for msg in delayed_messages:
+                                s_id, s_seq, s_data = msg
+                                stream_pending[s_id].append((s_seq, s_data))
+                                print(f"[Stream {s_id}] (seq {s_seq}) RELEASED: {s_data}")
+                            delayed_messages.clear()
+                        elif stream_id == 2:
+                            delayed_messages.append((stream_id, seq, data))
+                            print(f"[Stream {stream_id}] (seq {seq}) DELAYED: {data}")
+                        else:
+                            stream_pending[stream_id].append((seq, data))
+                            print(f"[Stream {stream_id}] (seq {seq}) QUEUED: {data}")
+
+                        for s_id in stream_pending:
+                            stream_pending[s_id].sort(key=lambda x: x[0])
+                            while stream_pending[s_id] and stream_pending[s_id][0][0] == stream_next_deliver[s_id]:
+                                msg = stream_pending[s_id].pop(0)
+                                print(f"[Stream {s_id}] (seq {msg[0]}) DELIVERED: {msg[1]}")
+                                stream_next_deliver[s_id] += 1
 
 
 if __name__ == '__main__':
