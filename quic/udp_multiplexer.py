1bda7f8c3f6982175b18b8f8dd49e7d9137be01b
abclark <aubs.bc@gmail.com>
2022-11-19 11:43:12 -0800

    feat: add ACK support to UDP multiplexer

--- quic/udp_multiplexer.py
+++ quic/udp_multiplexer.py
@@ -7,10 +7,39 @@ import protocols
 
 UDP_PORT = 9000
 
-stream_seqs = {1: 1, 2: 1, 3: 1}
+PACKET_DATA = 0x01
+PACKET_ACK = 0x02
+
 stream_next_deliver = {1: 1, 2: 1, 3: 1}
 stream_pending = {1: [], 2: [], 3: []}
 delayed_messages = []
+
+
+def send_udp(tun, src_ip, src_port, dest_ip, dest_port, payload):
+    udp = UDPHeader(
+        src_port=src_port,
+        dest_port=dest_port,
+        length=8 + len(payload),
+        checksum=0,
+        payload=payload
+    )
+    udp_bytes = udp.to_bytes(src_ip, dest_ip)
+
+    ip = IPHeader(
+        version=4,
+        ihl=5,
+        tos=0,
+        total_length=20 + len(udp_bytes),
+        identification=0,
+        flags_offset=0,
+        ttl=64,
+        protocol=protocols.PROTO_UDP,
+        checksum=0,
+        src_ip=src_ip,
+        dest_ip=dest_ip
+    )
+    ip_bytes = ip.to_bytes()
+    tun.write(ip_bytes + udp_bytes)
 
 
 def main():
@@ -18,7 +47,8 @@ def main():
 
     print("\nListening for UDP on port 9000...")
     print("Configure: sudo ifconfig utun<X> 10.0.0.1 10.0.0.1 netmask 255.255.255.0 up")
-    print("Test: echo -e '\\x01hello' | nc -u 10.0.0.1 9000\n")
+    print("Packet format: [type 1B][stream 1B][seq 2B][data...]")
+    print("  DATA=0x01, ACK=0x02\n")
 
     while True:
         packet_bytes = tun.read()
@@ -30,35 +60,34 @@ def main():
 
                 if udp_header.dest_port == UDP_PORT:
                     payload = udp_header.payload
-                    if len(payload) >= 1:
-                        stream_id = payload[0]
-                        data = payload[1:].decode('utf-8', errors='replace').strip()
+                    if len(payload) < 4:
+                        continue
 
-                        if stream_id not in stream_seqs:
-                            continue
+                    packet_type = payload[0]
+                    stream_id = payload[1]
+                    seq = int.from_bytes(payload[2:4], 'big')
 
-                        seq = stream_seqs[stream_id]
-                        stream_seqs[stream_id] += 1
+                    if stream_id not in stream_next_deliver:
+                        continue
 
-                        if data == "flush":
-                            for msg in delayed_messages:
-                                s_id, s_seq, s_data = msg
-                                stream_pending[s_id].append((s_seq, s_data))
-                                print(f"[Stream {s_id}] (seq {s_seq}) RELEASED: {s_data}")
-                            delayed_messages.clear()
-                        elif stream_id == 2:
-                            delayed_messages.append((stream_id, seq, data))
-                            print(f"[Stream {stream_id}] (seq {seq}) DELAYED: {data}")
-                        else:
-                            stream_pending[stream_id].append((seq, data))
-                            print(f"[Stream {stream_id}] (seq {seq}) QUEUED: {data}")
+                    if packet_type == PACKET_DATA:
+                        data = payload[4:].decode('utf-8', errors='replace').strip()
+                        print(f"[Stream {stream_id}] (seq {seq}) DATA: {data}")
 
-                        for s_id in stream_pending:
-                            stream_pending[s_id].sort(key=lambda x: x[0])
-                            while stream_pending[s_id] and stream_pending[s_id][0][0] == stream_next_deliver[s_id]:
-                                msg = stream_pending[s_id].pop(0)
-                                print(f"[Stream {s_id}] (seq {msg[0]}) DELIVERED: {msg[1]}")
-                                stream_next_deliver[s_id] += 1
+                        ack_payload = bytes([PACKET_ACK, stream_id]) + seq.to_bytes(2, 'big')
+                        send_udp(tun, ip_header.dest_ip, UDP_PORT, ip_header.src_ip, udp_header.src_port, ack_payload)
+                        print(f"[Stream {stream_id}] (seq {seq}) ACK sent")
+
+                        stream_pending[stream_id].append((seq, data))
+
+                        stream_pending[stream_id].sort(key=lambda x: x[0])
+                        while stream_pending[stream_id] and stream_pending[stream_id][0][0] == stream_next_deliver[stream_id]:
+                            msg = stream_pending[stream_id].pop(0)
+                            print(f"[Stream {stream_id}] (seq {msg[0]}) DELIVERED: {msg[1]}")
+                            stream_next_deliver[stream_id] += 1
+
+                    elif packet_type == PACKET_ACK:
+                        print(f"[Stream {stream_id}] (seq {seq}) ACK received")
 
 
 if __name__ == '__main__':
