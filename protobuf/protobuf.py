2e2cbd6d4c8ee1a84555c7ce85ba5b91919e8e09
abclark <aubs.bc@gmail.com>
2023-01-10 18:05:27 -0800

    protobuf varint decoding

--- protobuf/protobuf.py
+++ protobuf/protobuf.py
@@ -21,6 +21,30 @@ def encode_varint(n: int) -> bytes:
             break
     return bytes(result)
 
+
+def decode_varint(data: bytes, offset: int = 0) -> tuple[int, int]:
+    """
+    Decode a varint from bytes.
+
+    Args:
+        data: The bytes to decode from
+        offset: Starting position in data
+
+    Returns:
+        Tuple of (decoded_value, bytes_consumed)
+    """
+    result = 0
+    shift = 0
+    position = offset
+    while True:
+        byte = data[position]
+        result |= (byte & 0x7F) << shift
+        position += 1
+        if (byte & 0x80) == 0:
+            break
+        shift += 7
+    return (result, position - offset)
+
 # Quick test
 if __name__ == "__main__":
     # Test cases: (input, expected_output)
@@ -32,7 +56,14 @@ if __name__ == "__main__":
         (300, b'\xac\x02'),
     ]
 
+    print("Encoding:")
     for value, expected in tests:
         result = encode_varint(value)
         status = "✓" if result == expected else "✗"
-        print(f"{status} encode_varint({value}) = {result.hex() if result else None}, expected {expected.hex()}")
+        print(f"  {status} encode_varint({value}) = {result.hex() if result else None}, expected {expected.hex()}")
+
+    print("\nDecoding:")
+    for expected_value, encoded in tests:
+        value, length = decode_varint(encoded)
+        status = "✓" if value == expected_value else "✗"
+        print(f"  {status} decode_varint({encoded.hex()}) = {value}, expected {expected_value}")
