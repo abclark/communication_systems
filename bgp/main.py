e3c56b1fb86e55e6acea77b6358765fadca869cc
abclark <aubs.bc@gmail.com>
2023-03-07 16:42:10 -0800

    better bgp demo output

--- bgp/main.py
+++ bgp/main.py
@@ -1,108 +1,160 @@
+#!/usr/bin/env python3
+"""
+BGP Finite State Machine Demo
+
+Demonstrates the BGP peer session establishment process
+as defined in RFC 4271.
+"""
+
 import sys
 import time
 sys.path.insert(0, '.')
 from src.fsm import BGPPeer
 
-# --- Simulation Functions ---
+def print_header(text):
+    """Print a visually distinct section header."""
+    print(f"\n{'─' * 50}")
+    print(f"  {text}")
+    print(f"{'─' * 50}\n")
 
-def simulate_connect_retry_failure():
-    print("\n\n--- Starting BGP Simulation (Connect Retry Failure Path) ---", flush=True)
-    peer_c = BGPPeer("192.168.1.3")
-    peer_c.connect_retry_time = 2 # Use a slightly shorter time for faster cycles
+def print_event(event):
+    """Print an event being triggered."""
+    print(f"  ⚡ Event: {event}")
 
-    peer_c.handle_event("Start") # Peer C starts, transitions to CONNECT, starts ConnectRetryTimer
+def demo_happy_path():
+    """
+    Demonstrate successful BGP session establishment.
 
-    print(f"Waiting for a few cycles of CONNECT <-> ACTIVE transitions...", flush=True)
-    time.sleep(7) # Shortened for faster testing
+    Two peers go through:
+    IDLE → CONNECT → OPENSENT → OPENCONFIRM → ESTABLISHED
+    """
+    print_header("BGP Session Establishment (Happy Path)")
 
-    # Manually stop the timers and session to end the simulation
-    print("\nStopping the looping peer...", flush=True)
-    peer_c._stop_connect_retry_timer()
-    peer_c._transition_to_idle() # Explicitly move to IDLE to stop
+    print("  Creating two BGP peers...")
+    peer_a = BGPPeer("10.0.0.1")
+    peer_b = BGPPeer("10.0.0.2")
 
-    print("\n--- Connect Retry Failure Simulation Complete ---", flush=True)
+    # Disable hold timer for clean demo (won't expire during demo)
+    peer_a.hold_time = 60
+    peer_b.hold_time = 60
 
-def simulate_invalid_open():
-    print("\n\n--- Starting BGP Simulation (Invalid OPEN Message Path) ---", flush=True)
-    peer_d = BGPPeer("192.168.1.4")
+    time.sleep(0.5)
 
-    # Go through the initial steps to get the peer to OPENSENT
-    peer_d.handle_event("Start")
-    peer_d.handle_event("TcpConnectionSuccess")
-
-    # Now, simulate receiving an invalid OPEN message
-    print("\nSimulating receipt of an invalid OPEN message...", flush=True)
-    peer_d.handle_event("InvalidBGPOpenMessage")
-
-    # We need to manually stop the ConnectRetryTimer that was started in the CONNECT state
-    # because the FSM goes straight to IDLE without reaching a state where it's normally stopped.
-    peer_d._stop_connect_retry_timer()
-
-    print("\n--- Invalid OPEN Message Simulation Complete ---", flush=True)
-
-def simulate_open_delay_timer_expires():
-    print("\n\n--- Starting BGP Simulation (Open Delay Timer Expiration) ---", flush=True)
-    peer_e = BGPPeer("192.168.1.5")
-    peer_e.open_delay_time = 3 # Set short delay time for testing
-
-    # Go through the initial steps to get the peer to OPENSENT
-    peer_e.handle_event("Start")
-    peer_e.handle_event("TcpConnectionSuccess") # This starts the OpenDelayTimer
-
-    print(f"Waiting for Peer {peer_e.peer_ip} Open Delay Timer to expire in {peer_e.open_delay_time} seconds...", flush=True)
-    time.sleep(peer_e.open_delay_time + 1) # Wait for timer to expire
-
-    # We need to manually stop the ConnectRetryTimer that was started in the CONNECT state
-    # because the FSM goes straight to IDLE without reaching a state where it's normally stopped.
-    peer_e._stop_connect_retry_timer()
-
-    print("\n--- Open Delay Timer Expiration Simulation Complete ---", flush=True)
-
-
-# --- Main Execution ---
-
-def main():
-    # --- Happy Path Simulation ---
-    peer_a = BGPPeer("192.168.1.1")
-    peer_b = BGPPeer("192.168.1.2")
-
-    # For testing, set short timer values
-    peer_a.hold_time = 5
-    peer_b.hold_time = 5
-    peer_a.connect_retry_time = 3
-    peer_b.connect_retry_time = 3
-
-    print("--- Starting BGP Simulation (Happy Path) ---", flush=True)
+    # Step 1: Start
+    print_header("Step 1: Administrative Start")
+    print_event("Start")
     peer_a.handle_event("Start")
     peer_b.handle_event("Start")
+    time.sleep(0.8)
 
-    print("\n--- Simulating TCP Connection ---", flush=True)
+    # Step 2: TCP Connection
+    print_header("Step 2: TCP Connection Established")
+    print_event("TcpConnectionSuccess")
     peer_a.handle_event("TcpConnectionSuccess")
     peer_b.handle_event("TcpConnectionSuccess")
+    time.sleep(0.8)
 
-    print("\n--- Simulating OPEN Message Exchange ---", flush=True)
-    peer_a.handle_event("ValidBGPOpenMessage") 
+    # Step 3: OPEN Message
+    print_header("Step 3: Exchange OPEN Messages")
+    print("  Peers exchange: AS number, Hold Time, Router ID")
+    print_event("ValidBGPOpenMessage")
+    peer_a.handle_event("ValidBGPOpenMessage")
     peer_b.handle_event("ValidBGPOpenMessage")
+    time.sleep(0.8)
 
-    print("\n--- Simulating KEEPALIVE Message Exchange ---", flush=True)
+    # Step 4: KEEPALIVE
+    print_header("Step 4: Exchange KEEPALIVE Messages")
+    print("  Confirms both sides are ready")
+    print_event("BGPKeepaliveMessage")
     peer_a.handle_event("BGPKeepaliveMessage")
     peer_b.handle_event("BGPKeepaliveMessage")
+    time.sleep(0.5)
 
-    print("\n--- Simulation Complete (Happy Path) ---", flush=True)
+    print_header("✓ Sessions Established!")
+    print(f"  Peer A ({peer_a.peer_ip}): {peer_a.state.name}")
+    print(f"  Peer B ({peer_b.peer_ip}): {peer_b.state.name}")
+    print("\n  Now peers can exchange UPDATE messages with routes.\n")
 
-    # --- Hold Timer Failure Simulation ---
-    print("\n--- Testing Autonomous HoldTimer Expiration on Peer A ---", flush=True)
-    print(f"Waiting for Peer {peer_a.peer_ip} Hold Timer to expire in {peer_a.hold_time} seconds...", flush=True)
-    time.sleep(peer_a.hold_time + 1) # Wait slightly longer than hold_time
-    print("\n--- Autonomous HoldTimer Expiration Test Complete ---", flush=True)
+    # Cleanup
+    peer_a._stop_hold_timer()
+    peer_b._stop_hold_timer()
 
-    # --- Run Failure Simulations ---
-    simulate_connect_retry_failure()
-    simulate_invalid_open()
-    simulate_open_delay_timer_expires()
+    return peer_a, peer_b
 
-    print("\n\n--- All Simulations Finished ---", flush=True)
+def demo_connection_failure():
+    """
+    Demonstrate the CONNECT ↔ ACTIVE retry loop.
 
+    When TCP connection fails, BGP retries with exponential backoff.
+    """
+    print_header("Connection Failure (Retry Loop)")
+
+    peer = BGPPeer("10.0.0.3")
+    peer.connect_retry_time = 2  # Short timer for demo
+
+    print("  Peer starts but TCP connection keeps failing...")
+    print("  Watch the CONNECT ↔ ACTIVE loop:\n")
+
+    peer.handle_event("Start")
+    time.sleep(0.5)
+
+    # Simulate connection failures
+    for i in range(2):
+        time.sleep(1)
+        print_event("TcpConnectionFails")
+        peer.handle_event("TcpConnectionFails")
+        time.sleep(1.5)
+
+    print(f"\n  (In production, this continues until connection succeeds")
+    print(f"   or admin intervention)\n")
+
+    # Cleanup
+    peer._stop_connect_retry_timer()
+
+def demo_invalid_open():
+    """
+    Demonstrate handling of invalid OPEN message.
+
+    If OPEN message has bad parameters (wrong AS, bad hold time),
+    peer sends NOTIFICATION and returns to IDLE.
+    """
+    print_header("Invalid OPEN Message (Error Handling)")
+
+    peer = BGPPeer("10.0.0.4")
+
+    print("  Peer establishes TCP, but receives bad OPEN...\n")
+
+    peer.handle_event("Start")
+    time.sleep(0.5)
+    peer.handle_event("TcpConnectionSuccess")
+    time.sleep(0.5)
+
+    print("  ⚠ Received OPEN with wrong AS number!")
+    print_event("InvalidBGPOpenMessage")
+    peer.handle_event("InvalidBGPOpenMessage")
+
+    print(f"\n  Peer returns to IDLE, sends NOTIFICATION to remote.\n")
+
+    # Cleanup
+    peer._stop_connect_retry_timer()
+
+def main():
+    print("\n" + "═" * 50)
+    print("  BGP FINITE STATE MACHINE DEMO")
+    print("  RFC 4271 - Border Gateway Protocol")
+    print("═" * 50)
+
+    demo_happy_path()
+    time.sleep(1)
+
+    demo_connection_failure()
+    time.sleep(1)
+
+    demo_invalid_open()
+
+    print_header("Demo Complete")
+    print("  The BGP FSM ensures reliable session establishment")
+    print("  between autonomous systems across the internet.\n")
 
 if __name__ == "__main__":
     main()
--- bgp/path_selection.py
+++ bgp/path_selection.py
@@ -1,180 +1,226 @@
-# path_selection.py
+#!/usr/bin/env python3
+"""
+BGP Best Path Selection Algorithm
+
+Implements the decision process routers use to select the best
+route when multiple paths exist to the same destination.
+"""
 
 class Route:
-    """A simple class to represent a BGP route and its attributes."""
-    def __init__(self, prefix, as_path, next_hop, local_pref=100, origin='i', med=0, source='eBGP', router_id="0.0.0.0"):
+    """A BGP route with its path attributes."""
+    def __init__(self, name, prefix, as_path, next_hop, local_pref=100,
+                 origin='i', med=0, source='eBGP', router_id="0.0.0.0"):
+        self.name = name  # Friendly name for demo
         self.prefix = prefix
         self.as_path = as_path
         self.next_hop = next_hop
         self.local_pref = local_pref
-        self.origin = origin
+        self.origin = origin  # 'i'=IGP, 'e'=EGP, '?'=incomplete
         self.med = med
-        self.source = source # 'eBGP' or 'iBGP'
-        self.router_id = router_id # BGP Router ID of the advertising router
+        self.source = source  # 'eBGP' or 'iBGP'
+        self.router_id = router_id
 
     def __repr__(self):
-        """Provides a clean string representation of the Route object."""
-        return (f"Route(prefix='{self.prefix}', "
-                f"as_path={self.as_path}, "
-                f"next_hop='{self.next_hop}', "
-                f"local_pref={self.local_pref}, "
-                f"origin='{self.origin}', "
-                f"med={self.med}, "
-                f"source='{self.source}', "
-                f"router_id='{self.router_id}')")
+        return self.name
 
-# Dummy IGP cost function for simulation
+    def summary(self):
+        """One-line summary of the route."""
+        origin_names = {'i': 'IGP', 'e': 'EGP', '?': 'incomplete'}
+        return (f"{self.name}: AS_PATH={self.as_path or '(local)'}, "
+                f"LP={self.local_pref}, MED={self.med}, "
+                f"origin={origin_names.get(self.origin, self.origin)}, "
+                f"via {self.source}")
+
+
 def get_igp_cost(next_hop):
-    """
-    Simulates IGP cost to reach a next_hop.
-    In a real router, this would query the IGP routing table.
-    """
-    # Assign arbitrary costs for demonstration
-    if next_hop == "192.168.1.1": return 10
-    if next_hop == "192.168.1.2": return 5
-    if next_hop == "192.168.1.3": return 15
-    if next_hop == "0.0.0.0": return 0 # Locally originated, cost is 0
-    return 9999 # High cost for unknown next-hops
+    """Simulated IGP cost to reach next-hop (OSPF/IS-IS metric)."""
+    costs = {
+        "192.168.1.1": 10,
+        "192.168.1.2": 5,
+        "192.168.1.3": 15,
+        "0.0.0.0": 0,
+    }
+    return costs.get(next_hop, 9999)
 
 
-def select_best_path(routes):
+def select_best_path(routes, verbose=True):
     """
-    Implements the BGP Best Path Selection Algorithm.
-    Takes a list of Route objects and returns the single best Route.
+    BGP Best Path Selection Algorithm.
+
+    Returns the single best route from a list of candidates.
     """
     if not routes:
         return None
-    
+
+    def log_step(step_num, criterion, remaining, eliminated=None):
+        if not verbose:
+            return
+        print(f"\n  Step {step_num}: {criterion}")
+        if eliminated:
+            for r in eliminated:
+                print(f"    ✗ {r.name} — eliminated")
+        for r in remaining:
+            print(f"    ✓ {r.name} — still in")
+        if len(remaining) == 1:
+            print(f"\n  ══════════════════════════════════════")
+            print(f"  ★ WINNER: {remaining[0].name}")
+            print(f"  ══════════════════════════════════════")
+
+    candidates = routes[:]
+    step = 0
+
     # --- Step 1: Highest LOCAL_PREF ---
-    max_local_pref = max(route.local_pref for route in routes)
-    best_paths = [route for route in routes if route.local_pref == max_local_pref]
-    
-    if len(best_paths) == 1:
-        return best_paths[0]
-    
-    routes = best_paths
-    
+    step += 1
+    max_lp = max(r.local_pref for r in candidates)
+    eliminated = [r for r in candidates if r.local_pref != max_lp]
+    candidates = [r for r in candidates if r.local_pref == max_lp]
+    log_step(step, f"Highest LOCAL_PREF (best={max_lp})", candidates, eliminated)
+    if len(candidates) == 1:
+        return candidates[0]
+
     # --- Step 2: Locally Originated ---
-    locally_originated_paths = [route for route in routes if not route.as_path]
-    
-    if locally_originated_paths:
-        if len(locally_originated_paths) == 1:
-            return locally_originated_paths[0]
-        routes = locally_originated_paths
-    
+    step += 1
+    local = [r for r in candidates if not r.as_path]
+    if local:
+        eliminated = [r for r in candidates if r.as_path]
+        candidates = local
+        log_step(step, "Prefer locally originated (empty AS_PATH)", candidates, eliminated)
+        if len(candidates) == 1:
+            return candidates[0]
+    else:
+        if verbose:
+            print(f"\n  Step {step}: Locally originated — (no local routes, skip)")
+
     # --- Step 3: Shortest AS_PATH ---
-    min_as_path_len = min(len(route.as_path) for route in routes)
-    best_paths = [route for route in routes if len(route.as_path) == min_as_path_len]
+    step += 1
+    min_len = min(len(r.as_path) for r in candidates)
+    eliminated = [r for r in candidates if len(r.as_path) != min_len]
+    candidates = [r for r in candidates if len(r.as_path) == min_len]
+    log_step(step, f"Shortest AS_PATH (best={min_len} hops)", candidates, eliminated)
+    if len(candidates) == 1:
+        return candidates[0]
 
-    if len(best_paths) == 1:
-        return best_paths[0]
-    
-    routes = best_paths
-    
-    # --- Step 4: Lowest ORIGIN Code ---
-    origin_preference = {'i': 1, 'e': 2, '?': 3}
-    min_origin_value = min(origin_preference[route.origin] for route in routes)
-    best_paths = [route for route in routes if origin_preference[route.origin] == min_origin_value]
+    # --- Step 4: Lowest ORIGIN ---
+    step += 1
+    origin_pref = {'i': 1, 'e': 2, '?': 3}
+    origin_names = {'i': 'IGP', 'e': 'EGP', '?': 'incomplete'}
+    min_origin = min(origin_pref[r.origin] for r in candidates)
+    best_origin = [k for k, v in origin_pref.items() if v == min_origin][0]
+    eliminated = [r for r in candidates if origin_pref[r.origin] != min_origin]
+    candidates = [r for r in candidates if origin_pref[r.origin] == min_origin]
+    log_step(step, f"Lowest ORIGIN (best={origin_names[best_origin]})", candidates, eliminated)
+    if len(candidates) == 1:
+        return candidates[0]
 
-    if len(best_paths) == 1:
-        return best_paths[0]
-    
-    routes = best_paths
-    
     # --- Step 5: Lowest MED ---
-    min_med = min(route.med for route in routes)
-    best_paths = [route for route in routes if route.med == min_med]
+    step += 1
+    min_med = min(r.med for r in candidates)
+    eliminated = [r for r in candidates if r.med != min_med]
+    candidates = [r for r in candidates if r.med == min_med]
+    log_step(step, f"Lowest MED (best={min_med})", candidates, eliminated)
+    if len(candidates) == 1:
+        return candidates[0]
 
-    if len(best_paths) == 1:
-        return best_paths[0]
-    
-    routes = best_paths
-    
-    # --- Step 6: Prefer eBGP over iBGP paths ---
-    ebgp_paths = [route for route in routes if route.source == 'eBGP']
-    # ibgp_paths = [route for route in routes if route.source == 'iBGP'] # not needed if we just filter ebgp
-    
-    if ebgp_paths: # If there are eBGP paths, prefer them
-        if len(ebgp_paths) == 1:
-            return ebgp_paths[0]
-        routes = ebgp_paths # Proceed with tied eBGP paths
-    # else, routes already contains only iBGP paths or a mix that didn't prefer ebgp, so we proceed
+    # --- Step 6: eBGP over iBGP ---
+    step += 1
+    ebgp = [r for r in candidates if r.source == 'eBGP']
+    if ebgp:
+        eliminated = [r for r in candidates if r.source != 'eBGP']
+        candidates = ebgp
+        log_step(step, "Prefer eBGP over iBGP", candidates, eliminated)
+        if len(candidates) == 1:
+            return candidates[0]
+    else:
+        if verbose:
+            print(f"\n  Step {step}: eBGP over iBGP — (all same type, skip)")
 
-    # --- Step 7: Lowest IGP Cost to NEXT_HOP ---
-    if len(routes) > 1: # Only apply if there are multiple routes left
-        min_igp_cost = min(get_igp_cost(route.next_hop) for route in routes)
-        best_paths = [route for route in routes if get_igp_cost(route.next_hop) == min_igp_cost]
+    # --- Step 7: Lowest IGP cost to next-hop ---
+    step += 1
+    min_cost = min(get_igp_cost(r.next_hop) for r in candidates)
+    eliminated = [r for r in candidates if get_igp_cost(r.next_hop) != min_cost]
+    candidates = [r for r in candidates if get_igp_cost(r.next_hop) == min_cost]
+    log_step(step, f"Lowest IGP cost to next-hop (best={min_cost})", candidates, eliminated)
+    if len(candidates) == 1:
+        return candidates[0]
 
-        if len(best_paths) == 1:
-            return best_paths[0]
-        routes = best_paths
-    
-    # --- Step 8: Final Tie-Breakers (Lowest BGP Router ID) ---
-    if len(routes) > 1: # If multiple paths remain (tied after all previous steps)
-        min_router_id = min(route.router_id for route in routes)
-        best_paths = [route for route in routes if route.router_id == min_router_id]
-        
-        # If there's still a tie after Router ID, we pick the first one (deterministic but arbitrary)
-        return best_paths[0]
-    
-    # If only one path remained from previous steps, return it
-    return routes[0] # Return the single remaining route
+    # --- Step 8: Lowest Router ID ---
+    step += 1
+    min_rid = min(r.router_id for r in candidates)
+    eliminated = [r for r in candidates if r.router_id != min_rid]
+    candidates = [r for r in candidates if r.router_id == min_rid]
+    log_step(step, f"Lowest Router ID (best={min_rid})", candidates, eliminated)
+
+    return candidates[0]
 
 
-# --- Sample Data for Testing ---
+def demo():
+    """Demonstrate the path selection algorithm."""
+    print("\n" + "═" * 50)
+    print("  BGP BEST PATH SELECTION DEMO")
+    print("  Which route wins for 8.8.8.0/24?")
+    print("═" * 50)
+
+    # Create competing routes to Google DNS
+    routes = [
+        Route(
+            name="Route A (via AS 65001)",
+            prefix="8.8.8.0/24",
+            as_path=[65001],
+            next_hop="192.168.1.1",
+            local_pref=100,
+            origin='i',
+            med=10,
+            source='eBGP',
+            router_id="1.1.1.1"
+        ),
+        Route(
+            name="Route B (via AS 65002→65001)",
+            prefix="8.8.8.0/24",
+            as_path=[65002, 65001],
+            next_hop="192.168.1.2",
+            local_pref=100,
+            origin='i',
+            med=5,
+            source='eBGP',
+            router_id="2.2.2.2"
+        ),
+        Route(
+            name="Route C (via AS 65003)",
+            prefix="8.8.8.0/24",
+            as_path=[65003],
+            next_hop="192.168.1.3",
+            local_pref=90,  # Lower preference
+            origin='i',
+            med=0,
+            source='eBGP',
+            router_id="3.3.3.3"
+        ),
+        Route(
+            name="Route D (via AS 65004)",
+            prefix="8.8.8.0/24",
+            as_path=[65004],
+            next_hop="192.168.1.1",
+            local_pref=100,
+            origin='e',  # EGP origin (less preferred than IGP)
+            med=10,
+            source='eBGP',
+            router_id="4.4.4.4"
+        ),
+    ]
+
+    print("\n  Candidate Routes:")
+    print("  " + "─" * 46)
+    for r in routes:
+        print(f"  • {r.summary()}")
+    print("  " + "─" * 46)
+    print("\n  Running selection algorithm...")
+
+    winner = select_best_path(routes)
+
+    print(f"\n  Final selection: {winner.name}")
+    print(f"  Reason: Shortest AS_PATH (1 hop) + IGP origin")
+    print()
+
+
 if __name__ == "__main__":
-    # All routes are for the same destination prefix 8.8.8.0/24
-    print("--- Sample Route Data ---")
-
-    route1 = Route(
-        prefix="8.8.8.0/24",
-        as_path=[65002, 65001],
-        next_hop="192.168.1.1",
-        local_pref=100,
-        origin='i',
-        med=0,
-        source='eBGP', # Learned from an external peer
-        router_id="1.1.1.1" # Router ID of advertising peer
-    )
-
-    route2 = Route(
-        prefix="8.8.8.0/24",
-        as_path=[65003, 65001], # Same AS_PATH length as route1
-        next_hop="192.168.1.2",
-        local_pref=150, # Higher LOCAL_PREF
-        origin='i',
-        med=0,
-        source='eBGP',
-        router_id="2.2.2.2"
-    )
-
-    route3 = Route(
-        prefix="8.8.8.0/24",
-        as_path=[65004, 65005, 65001], # Longer AS_PATH
-        next_hop="192.168.1.3",
-        local_pref=100,
-        origin='i',
-        med=10, # Higher MED (less preferred)
-        source='eBGP',
-        router_id="3.3.3.3"
-    )
-
-    route4 = Route(
-        prefix="8.8.8.0/24",
-        as_path=[], # Locally originated
-        next_hop="0.0.0.0", # Usually 0.0.0.0 for locally originated
-        local_pref=200, # Highest LOCAL_PREF
-        origin='i',
-        med=0,
-        source='iBGP', # Representing local origin
-        router_id="0.0.0.0" # Router's own ID
-    )
-
-    all_routes = [route1, route2, route3, route4]
-
-    for i, route in enumerate(all_routes):
-        print(f"Route {i+1}: {route}")
-    print("\n")
-
-    best_route = select_best_path(all_routes)
-    print(f"Best Path Selected: {best_route}")
+    demo()
